be2ea2d9468ffa41b246281e3bd851c729523550
PetrykSergii <148215871+PetrykSergii@users.noreply.github.com>
2024-07-28 11:06:33 +0300

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Запуск ZAP в режиме командной строки с тестовым планом
+zap.sh -daemon -config api.disablekey=true -port 8090
+zap-cli quick-scan --self-contained --start-options '-config api.disablekey=true' http://your-web-app-url
+zap-cli report -o zap_report.html -f html
+
+# Удаление ZAP
+zap-cli shutdown
